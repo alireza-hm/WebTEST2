d3d97295ff3d402b219d61a8d38416b6e1f04d51
alireza-hm <77230472+alireza-hm@users.noreply.github.com>
2023-06-08 23:45:31 +0330

    alireza

--- WebTEST2/Statics/Cstring.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebTEST2.Statics
-{
-    public static class Cstring
-    {
-        public static string Value = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=DBMSTEST;Integrated Security=True;MultipleActiveResultSets=True";
-    }
-}
